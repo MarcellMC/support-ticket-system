a78e687e56adc19292231eb98a0f5dab573bb39c
Marc Moiseev <marcell89@gmail.com>
2024-03-16 15:22:27 +0200

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tickets do
+    collection do
+      get 'manage'
+    end
+  end
+
+  root 'tickets#index'
 end
