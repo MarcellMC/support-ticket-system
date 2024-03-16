09bf0b787b56c8ba699a2e7a42ade9362d371952
Marc Moiseev <marcell89@gmail.com>
2024-03-16 12:06:34 +0200

    Add `#to_csv` method

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,19 @@
+##
+# Support ticket model
 class Ticket < ApplicationRecord
   validates :name, :email, :subject, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   enum status: [:new, :pending, :resolved]
+
+  # Generate CSV file
+  def self.to_csv
+    attributes = %w{name email status subject content}
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |ticket|
+        csv << attributes.map { |attr| ticket.send(attr) }
+      end
+    end
+  end
 end
