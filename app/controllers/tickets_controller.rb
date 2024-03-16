302a455ecf86a57295723c85ec9872b2c35a3021
Marc Moiseev <marcell89@gmail.com>
2024-03-16 14:13:55 +0200

    Create TicketsController

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,20 @@
+class TicketsController < ApplicationController
+  def create
+    @ticket = Ticket.new(ticket_params)
+
+    if @ticket.save
+      File.open('tickets.csv', 'a') do |file|
+        file.puts @ticket.to_csv
+      end
+      redirect_to @ticket, notice: 'Ticket was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:name, :email, :subject, :content)
+  end
+end
