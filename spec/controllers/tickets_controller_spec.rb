9b4a032fa66a2c277f960a5349db4b05f6a6beba
Marc Moiseev <marcell89@gmail.com>
2024-03-17 10:07:47 +0200

    Add test for index view of TicketController

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
   describe 'routing' do
     it 'routes to #index' do
       expect(get: '/tickets').to route_to('tickets#index')
