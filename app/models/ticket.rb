8a02822063f4ec65643079adb9f3a80966086209
Marc Moiseev <marcell89@gmail.com>
2024-03-17 20:12:11 +0200

    Update CSV creation and related tests

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,19 +1,18 @@
+require 'csv'
+
 ##
 # Support ticket model
 class Ticket < ApplicationRecord
+  CSV_FILE_PATH = Rails.root.join('data', 'tickets.csv')
+
   validates :name, :email, :subject, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   enum ticket_status: [:new_ticket, :pending, :resolved]
 
-  # Generate CSV file
-  def self.to_csv
-    attributes = %w{name email status subject content}
-    CSV.generate(headers: true) do |csv|
-      csv << attributes
-
-      all.each do |ticket|
-        csv << attributes.map { |attr| ticket.send(attr) }
-      end
+  def save_to_csv(filepath: nil)
+    filepath ||= CSV_FILE_PATH
+    CSV.open(filepath, 'a+') do |csv|
+      csv << [name, email, subject, content]
     end
   end
 end
--- spec/models/csv_storage_spec.rb
+++ spec/models/csv_storage_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
   describe '#save_to_csv' do
+    let(:temp_csv_file) { 'temp_tickets.csv' }
+
+    after(:each) do
+      File.delete(temp_csv_file) if File.exist?(temp_csv_file) # Delete temporary CSV file after each test
+    end
+
     it 'saves ticket details to CSV file' do
       ticket = Ticket.new(
         name: 'John Doe',
@@ -9,14 +15,15 @@ RSpec.describe Ticket, type: :model do
         subject: 'Test Ticket',
         content: 'Test Content'
       )
-      ticket.save_to_csv
+      ticket.save_to_csv(filepath: temp_csv_file)
 
-      csv_data = CSV.read(Ticket::CSV_FILE_PATH, headers: true)
-      expect(csv_data.length).to eq(1)
-      expect(csv_data[0]['Name']).to eq('John Doe')
-      expect(csv_data[0]['Email']).to eq('john.doe@example.com')
-      expect(csv_data[0]['Subject']).to eq('Test Ticket')
-      expect(csv_data[0]['Content']).to eq('Test Content')
+      last_line = `tail -n 1 #{Ticket::CSV_FILE_PATH}`
+      parsed_row = CSV.parse_line(last_line)
+
+      expect(parsed_row[0]).to eq('John Doe')
+      expect(parsed_row[1]).to eq('john.doe@example.com')
+      expect(parsed_row[2]).to eq('Test Ticket')
+      expect(parsed_row[3]).to eq('Test Content')
     end
   end
 end
