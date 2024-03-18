e34d794cff0d6229bb902a8a95776adc427b1a82
Marc Moiseev <marcell89@gmail.com>
2024-03-18 03:29:19 +0200

    Install and configure Capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
+  require 'capybara/rspec'
+  require 'capybara/webkit'
+
+  Capybara.javascript_driver = :webkit
+
 end
