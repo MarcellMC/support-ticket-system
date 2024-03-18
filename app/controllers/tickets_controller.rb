63175bd8a095a9789be6baa593200193303c2b00
Marc Moiseev <marcell89@gmail.com>
2024-03-18 18:00:22 +0200

    Implement #edit, #update, #destroy actions

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -11,6 +11,19 @@ class TicketsController < ApplicationController
     @tickets = Ticket.all
   end
 
+  def edit
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+    if @ticket.update(ticket_params)
+      redirect_to manage_tickets_path, notice: 'Ticket updated successfully'
+    else
+      render :edit
+    end
+  end
+
   def create
     @ticket = Ticket.new(ticket_params)
 
@@ -20,6 +33,12 @@ class TicketsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
+    redirect_to manage_tickets_path, notice: 'Ticket deleted successfully'
   end
 
   private
