b7b240b5f4fa4722ac8454a145c4bcd98709b7c7
Marc Moiseev <marcell89@gmail.com>
2024-03-18 03:30:37 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'tickets#index'
+
   resources :tickets do
     collection do
       get 'manage'
     end
   end
 
-  root 'tickets#index'
 end
