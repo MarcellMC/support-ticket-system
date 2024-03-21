efd2f2785d97fd9ddf499224542db4b2f52ebc08
Marc Moiseev <marcell89@gmail.com>
2024-03-21 19:09:34 +0200

    Fix the spec for Edit Ticket template

--- spec/views/edit_ticket_view_spec.rb
+++ spec/views/edit_ticket_view_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'tickets/edit.html.erb', type: :view do
   it 'renders the edit ticket form' do
-    ticket = Ticket.new(name: 'John Doe', email: 'john@example.com', subject: 'Ticket 1', content: 'This is ticket 1')
+    ticket = Ticket.create(name: 'John Doe', email: 'john@example.com', subject: 'Ticket 1', content: 'This is ticket 1')
     assign(:ticket, ticket)
 
     render
 
-    expect(rendered).to have_selector('form[action=?][method=?]', ticket_path(ticket), 'post') do |form|
-      expect(form).to have_selector('input[name=?][type=?]', 'ticket[name]', 'text')
-      expect(form).to have_selector('input[name=?][type=?]', 'ticket[email]', 'text')
-      expect(form).to have_selector('input[name=?][type=?]', 'ticket[subject]', 'text')
-      expect(form).to have_selector('textarea[name=?]', 'ticket[content]')
-      expect(form).to have_selector('input[type=?]', 'submit')
+    expect(rendered).to have_selector('form') do |form|
+      expect(form).to have_selector('input[name="ticket[name]"][type="text"]')
+      expect(form).to have_selector('input[name="ticket[email]"][type="text"]')
+      expect(form).to have_selector('input[name="ticket[subject]"][type="text"]')
+      expect(form).to have_selector('textarea[name="ticket[content]"]')
+      expect(form).to have_selector('input[type="submit"]')
     end
   end
 end
